16fb41f8a0e70cf94b840a9a5376f57786905c91
Tom Richter <tom.richter@outlook.com>
2020-02-02 02:43:53 -0500

    Added Autofac Dependency Injection

--- EVE.Windower.UI/App.xaml.cs
+++ EVE.Windower.UI/App.xaml.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using EVE.Windower.UI;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Windows;
 
 namespace EVE.Windower
@@ -13,5 +12,41 @@ namespace EVE.Windower
     /// </summary>
     public partial class App : Application
     {
+        private IServiceProvider _services;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            _services = ConfigureServiceProvider();
+
+            Current.MainWindow = _services.GetService<MainWindow>();
+            Current.MainWindow.Show();
+        }
+
+        /// <summary>
+        /// Configures dependency injection service provider.
+        /// </summary>
+        /// <returns>Configured service provider.</returns>
+        private IServiceProvider ConfigureServiceProvider()
+        {
+            // Register Microsoft.Extensions-compatible types
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging();
+
+            // Populate above into Autofac container
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.Populate(serviceCollection);
+
+            // Register custom types here (will override types registered above!)
+            containerBuilder.RegisterType<MainWindow>();
+            containerBuilder.RegisterType<MainWindowViewModel>();
+
+            // Build service provider
+            var container = containerBuilder.Build();
+            var serviceProvider = new AutofacServiceProvider(container);
+
+            return serviceProvider;
+        }
     }
 }
---
+++ EVE.Windower.UI/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace EVE.Windower.UI
+{
+    public class MainWindowViewModel
+    {
+        readonly ILogger<MainWindowViewModel> _logger;
+
+        public MainWindowViewModel(ILogger<MainWindowViewModel> logger)
+        {
+            _logger = logger;
+
+            _logger.LogDebug("MainWindowViewModel created.");
+        }
+    }
+}
